faf803b3d40032e3df70d470f6fa19878da492ca
GaiaStromokov <gaiamitchener@gmail.com>
2025-01-31 12:38:33 -0800

    Delete v_new directory

--- v_new/DICTS/DICT_IMPORT.py
+++
@@ -1,4 +0,0 @@
-from DICTS.CLASS_DATA import *
-from DICTS.RACE_DATA import *
-from DICTS.TRAIT_DATA import *
-from DICTS.BG_DATA import *
--- v_new/config/sub_import.py
+++
@@ -1,4 +0,0 @@
-from Classes.Player import *
-from Classes.UIX import *
-
-from DICTS.DICT_IMPORT import *
